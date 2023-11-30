5e52a9d7f7dfee31a0d02b1b13c407c164c1761c
FrancoGuevara <francoguevara93@gmail.com>
2023-11-30 00:31:03 -0300

    Inventory with pagination added

--- src/pages/Inventory/Inventory.jsx
+++ src/pages/Inventory/Inventory.jsx
@@ -1,9 +1,70 @@
-import { Typography } from "antd";
+import { Avatar, Rate, Space, Table, Typography } from "antd";
+import { useEffect, useState } from "react";
+import { getInventory } from "../../API/db";
 
 export default function Inventory(){
+    const [loading, setLoading] = useState(false);
+    const [dataSource, setDataSource] = useState([]);
+
+    useEffect(()=>{
+        setLoading(true);
+
+        getInventory().then((res)=>{
+            setDataSource(res.products)
+            setLoading(false);
+        })
+    }, [])
+
     return(
-        <>
-                <Typography.Title level={4}>Inventory</Typography.Title>
-        </>
+        <Space size={20} direction="vertical">
+            <Typography.Title level={4}>Inventory</Typography.Title>
+            <Table columns={[
+                {
+                    title: 'Thumbnail',
+                    dataIndex: 'thumbnail',
+                    render: (link) =>{
+                        return <Avatar src={link}/>
+                    }
+                },
+                {
+                    title: 'Title',
+                    dataIndex: 'title'
+                },
+                {
+                    title: 'Price',
+                    dataIndex: 'price',
+                    render:(value)=>{
+                        return <span>${value}</span>
+                    }
+                },
+                {
+                    title: 'Rating',
+                    dataIndex: 'rating',
+                    render:(rating)=>{
+                        return <Rate value={rating} allowHalf disabled/>
+                    }
+                },
+                {
+                    title: 'Stock',
+                    dataIndex: 'stock'
+                },
+                {
+                    title: 'Brand',
+                    dataIndex: 'brand'
+                },
+                {
+                    title: 'Category',
+                    dataIndex: 'category'
+                },
+                ]}
+                dataSource={dataSource}
+                loading={loading}
+                pagination={{
+                    pageSize:5
+                }}
+                >
+
+            </Table>
+        </Space>
     )
 }
